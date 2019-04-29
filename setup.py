476e53342813d9e151aecf05d0e28aa51b582886
Jackson Cornelius <jackson.cornelius@cfdrc.com>
2019-04-29 10:24:46 -0500

    Add some more initial stuff

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='kalman',
+    version='0.0',
+    author='Jackson Cornelius',
+    author_email="jawajack@gmail.com",
+    description='Kalman Filtering in PyTorch',
+    url='https://github.com/jawajack/kalman-pytorch',
+    packages=['kalman',],
+    license='MIT',
+    long_description=open('README.md').read()
+)
